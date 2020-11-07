8cabc43c65f90ca2a57a6993a6cdc9edd57a5943
josecortesss <jocobe93@gmail.com>
2020-11-07 10:23:13 +0100

    Commit inicial con Eclipse. Close #1

---
+++ NOVIEMBRE_Aplicacion/src/main/java/com/jose/demo/NoviembreAplicacionApplication.java
@@ -0,0 +1,13 @@
+package com.jose.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoviembreAplicacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoviembreAplicacionApplication.class, args);
+	}
+
+}
